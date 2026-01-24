624fcc9cb1667e0311140404b63c79018082fe64
Florian Schunk <florian.git@repatr.de>
2026-01-24 20:09:54 +0000

    update default.nix

--- default.nix
+++ default.nix
@@ -7,34 +7,20 @@
   imports = [
     dream2nix.modules.dream2nix.rust-cargo-lock
     dream2nix.modules.dream2nix.rust-cargo-vendor
-    dream2nix.modules.dream2nix.rust-crane
+    dream2nix.modules.dream2nix.buildRustPackage
   ];
 
-  deps = {nixpkgs, ...}: {
-    inherit (nixpkgs) fetchFromGitHub iconv;
-  };
-
-  name = lib.mkForce "ripgrep";
-  version = lib.mkForce "13.0.0";
-
-  # options defined on top-level will be applied to the main derivation (the derivation that is exposed)
   mkDerivation = {
-    # define the source root that contains the package we want to build.
-    src = config.deps.fetchFromGitHub {
-      owner = "BurntSushi";
-      repo = "ripgrep";
-      rev = config.version;
-      sha256 = "sha256-udEh+Re2PeO3DnX4fQThsaT1Y3MBHFfrX5Q5EN2XrF0=";
-    };
-    buildInputs = lib.optionals config.deps.stdenv.isDarwin [config.deps.iconv];
+    src = ./.;
   };
 
-  rust-crane = {
-    buildProfile = "dev";
-    buildFlags = ["--verbose"];
-    runTests = false;
-    depsDrv = {
-      # options defined here will be applied to the dependencies derivation
-    };
+  deps = {nixpkgs, ...}: {
+    inherit
+      (nixpkgs)
+      stdenv
+      ;
   };
+
+  name = "declaro";
+  version = "0.1.0";
 }
