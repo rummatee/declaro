8cb701ff57fc1c9852187e764a6b49ef38029269
Florian Schunk <florian.git@repatr.de>
2026-02-15 21:35:27 +0000

    add animations

--- src/router.rs
+++ src/router.rs
@@ -1,17 +1,43 @@
 use dioxus::prelude::*;
+use dioxus_motion::prelude::*;
+use dioxus_motion::transitions::page_transitions::TransitionVariantResolver;
 
 use crate::components::NodeUI;
 use crate::ast::AstPath;
 
-#[derive(Clone, Debug, PartialEq, Routable)]
+#[derive(Clone, Debug, PartialEq, Routable, MotionTransitions)]
 pub enum Route {
 
-    #[route("/node/:path")]
-    NodeUI { path: AstPath },
+    #[layout(Wrapper)]
+        #[route("/node/:path")]
+        #[transition(SlideRight)]
+        NodeUI { path: AstPath },
 
-    #[route("/")]
-    Home {}
+        #[route("/")]
+        Home {}
 
+}
+
+#[component]
+pub fn Wrapper() -> Element {
+    let resolver: TransitionVariantResolver<Route> = std::rc::Rc::new(|from, to| {
+        match (from, to) {
+            (Route::NodeUI { path: from_path }, Route::NodeUI { path: to_path }) => {
+                if from_path.indices.len() > to_path.indices.len() {
+                    TransitionVariant::SlideRight
+                } else if from_path.indices.len() < to_path.indices.len() {
+                    TransitionVariant::SlideLeft
+                } else {
+                    TransitionVariant::Fade
+                }
+            }
+            _ => TransitionVariant::Fade
+        }
+    });
+    use_context_provider(|| resolver);
+    rsx! {
+        AnimatedOutlet::<Route> {}
+    }
 }
 
 #[component]
